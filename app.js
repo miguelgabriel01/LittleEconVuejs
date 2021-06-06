735ae6274d8b969891cb8190763f079fe983930b
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-06 14:13:08 -0300

    Criação do fatch de produto para buscar dados especificos de um item pelo id

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const vm = new Vue({
     data(){
         return{
             produtos:[],//variavel que vai armazenar os dados sobre o produto
+            produto:true,//responsavel pela exibição do modal do produto
         }
     },
     methods: {
         fetchProdutos(){
+            //responsavel por buscar todos os produtos 
             fetch('./api/produtos.json')//fazemos a requisição a api atraves da url
             .then(resposta => resposta.json())//transformaos a resposta em um JSON
             .then(resposta => {
                 this.produtos = resposta//inserimos o valor da requisição em uma variavel pre definida
+            })
+        },
+        fetchProduto(id){//esse fatch vai receber como parametro o id do produto
+            //responsavel por buscar apenas um produto especifico pelo ID
+            fetch(`./api/produtos/${id}/dados.json`)//fazemos uma requisição usando como parametro o id do produto
+            .then(resposta => resposta.json())//transformamos a resposta em json
+            .then(resposta => {
+               this.produto = resposta//atribuimos a var produto(no singular) o valor da requisição
             })
         }
     },
