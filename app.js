9300a6f5e8e4196a2c957c5f3b84d1a914cf8f58
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-06 19:56:11 -0300

    finalização do projeto com refatoração urgente

--- app.js
+++ app.js
@@ -1,127 +1,103 @@
 const vm = new Vue({
-    el:'#app',
-    data(){
-        return{
-            produtos:[],//variavel que vai armazenar os dados sobre o produto
-            produto:false,//responsavel pela exibição do modal do produto
-            //carrinhoTotal:0,//valor inicial do carrinho de compras
-            carrinho:[],//neste item vão estar os produtos add ao carinho
-            mensagemAlerta:'Item adicionado',
-            alertaAtivo:false,//valor que sera usado para iniciar o alerta
-
-            
-        }
-    },
-    computed:{
-        carrinhoTotal(){
-        //responsavel por computar o valor total de itens add ao carrinho
-        let total = 0
-        if(this.carrinho.length){
-            this.carrinho.forEach(item => {
-                total += item.preco
-            })
-        }
-        return total
-        } 
-    },
-    methods: {
-        fetchProdutos(){
-            //responsavel por buscar todos os produtos 
-            fetch('./api/produtos.json')//fazemos a requisição a api atraves da url
-            .then(resposta => resposta.json())//transformaos a resposta em um JSON
-            .then(resposta => {
-                this.produtos = resposta//inserimos o valor da requisição em uma variavel pre definida
-            })
-        },
-        fetchProduto(id){//esse fatch vai receber como parametro o id do produto
-            //responsavel por buscar apenas um produto especifico pelo ID
-            fetch(`./api/produtos/${id}/dados.json`)//fazemos uma requisição usando como parametro o id do produto
-            .then(resposta => resposta.json())//transformamos a resposta em json
-            .then(resposta => {
-               this.produto = resposta//atribuimos a var produto(no singular) o valor da requisição
-            })
-        },
-        fecharModal({ target, currentTarget }) {
-            if (target === currentTarget) this.produto = false;
-          },
-          adicionarItem(){
-              //responsavel p0or adicionar itens ao carrinho
-              this.produto.estoque--//removemos o numero de itens disponivel
-              const {id,nome,preco} = this.produto//desestruturamos em uma const as variaveis que queremos
-              this.carrinho.push({id,nome,preco})//add a var carrinho os nomes do produto
-              
-              //aqui usamos o metodo de add uma msg ao alerta
-              this.alerta(`${nome} adicionado ao carrinho`)//passamos como parametro para a função a msg
-
-         },
-          removerItem(index){//recebe como parametr o index do item add ao carrinho
-              //responsavel por remover os itens do carrinho
-              this.carrinho.splice(index,1)//apaga de acordo com o index informado
-          },
-          checarLocalStorage(){
-              //metodo responsavel por verificar se existem dados salvos no localStorage
-              if(window.localStorage.carrinho){
-                  this.carrinho = JSON.parse(window.localStorage.carrinho)
-                  //o JSON.parse transforma uma string em array
-              }
-          },
-          alerta(mensagem){//recebe como parametro uma msg de acordo com a interação do usuario
-              //responsavel por modificar a msg do alerta
-              this.mensagemAlerta = mensagem//mensagem que é recebida como parametro
-              this.alertaAtivo = true//modifica o valor do alerta
-
-              //retirar o alerta da tela apos um certo tempo
-              setTimeout(() => {
-                  this.alertaAtivo = false
-              },3000)
-          },
-          router(){
-              //responsavel por favor com que a url possa serr aberta em outras abas
-              const hash = document.location.hash
-              if(hash){
-                  this.fetchProduto(hash.replace('#',''))
-              }
-          } 
-    },
-    watch:{
-     produto(){
-      //responsavel por verificar se o preoduto foi alterado e mudar a url
-      
-      //alteramos o titulo da pagina
-      document.title = this.produto.nome || "tecnon"
-
-      //alteramos a url
-      const hash = this.produto.id
-      history.pushState(null,null, `#${hash}`)
-     },
-     carrinho(){//sempre a propiedade é a qual vc quer avaliar se aconteceu mudanças
-      //responsavel por verificar se o carrinho aconteceu mudancas e salvar no nlocalStortage
-      window.localStorage.carrinho = JSON.stringify(this.carrinho)//maneira mais facil de salvar no localStorage n exitse 
-     //o JSON.stringtfy transforma um array em string
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    carrinho: [],
+    carrinhoAtivo: false,
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false,
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
     }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        })
+      }
+      return total;
+    }
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        })
     },
-    created() {
-        //asim que a plicação for iniciada, o created ira chamar o methodo que faz a requisição a api
-        this.fetchProdutos()//o metodo é chamado
-
-        //assim que o app for iniciado ele ira chamar a função que verifica se existem valores no localStorage
-        this.checarLocalStorage()//o metodo é chamado
-
-        //assim que iniciado, já podera pegar a rota 
-        this.router()
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
+        })
     },
-    filters:{
-        //filtro criado para a validação do preco dos produtos
-        numeropreco(valor){
-          //o valor passado como paraemtro é o dado que esta no html e que vai usar o filtro
-          //ex:
-          //{{produyo.preco}} = valor
-          //ou
-          //{{produto.marca}} = valor
-
-          return valor.toLocaleString('pt-BR',{style:"currency", currency: 'BRL'})
-        }
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
     },
-
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho.`);
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho)
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+    },
+    compararEstoque() {
+      const items = this.carrinho.filter(({ id }) => id === this.produto.id);
+      this.produto.estoque -= items.length;
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if (hash)
+        this.fetchProduto(hash.replace("#", ""));
+    }
+  },
+  watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+      if (this.produto) {
+        this.compararEstoque();
+      }
+    },
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    }
+  },
+  created() {
+    this.fetchProdutos();
+    this.router();
+    this.checarLocalStorage();
+  }
 })
-    
